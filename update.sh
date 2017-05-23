7b20a377e2f48586c6ffad89fed266484f055951
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2017-05-23 16:46:04 -0600

    updated vimrc

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Pulling vimSettings repo..."
-git pull
-
-echo "Replacing ~/.vimrc with vimSettings/vimrc..."
-cp ./vimrc ~/.vimrc
-
-echo "done"
--- vimrc
+++ vimrc
@@ -10,5 +10,6 @@ set shiftwidth=4
 set tabstop=4 
 
 syntax enable
-set background=dark
+set t_Co=16
+let g:solarized_termcolors=16
 colorscheme solarized
