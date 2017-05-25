313d63ede8081bae469ec3c8e3037567477a581e
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2017-05-25 08:52:24 -0600

    updated vimrc

--- configure.sh
+++ configure.sh
@@ -6,4 +6,7 @@ git pull
 echo "Replacing ~/.vimrc with vimSettings/vimrc..."
 cp ./vimrc ~/.vimrc
 
+#echo "exporting PS1..."
+#export PS1="[\u@\h \w]\\$ \[$(tput sgr0)\]"
+
 echo "done"
--- vimrc
+++ vimrc
@@ -1,10 +1,8 @@
 syntax enable
 set noswapfile
-set number
 set textwidth=80
 set hlsearch
 set autoindent
 set expandtab
 set shiftwidth=4
 set tabstop=4 
-
