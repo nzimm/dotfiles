38770a4619a815de4eac55a5fade3c3a19537d98
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2018-09-26 10:30:30 -0600

    Added symbols, reduced network activity from vpn script

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -5,10 +5,9 @@
 #    geoip
 
 VPN_IFACE=$(nmcli con show | grep tun0 | cut -d ' ' -f1)
-PUBLIC=$(curl -s "ifconfig.me/ip")
 
 if [[ "$VPN_IFACE" == "tun0" ]]; then
-    STATUS="$(geoiplookup ${PUBLIC} | cut -f 2 -d ',')"
+    STATUS="$(geoiplookup $(curl -s "ifconfig.me/ip") | cut -f 2 -d ',')"
     COLOR="#00FF00"
 else
     STATUS="OFF"
