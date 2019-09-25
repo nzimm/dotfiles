65456eb97244b100367ea9a39650ace3e12fecad
nzimm <nizi2734@colorado.edu>
2019-09-25 13:43:09 -0600

    print public ip geolocation regardless of vpn status

--- config/polybar/scripts/vpn-status.py
+++ config/polybar/scripts/vpn-status.py
@@ -11,52 +11,60 @@ import subprocess
 import sys
 from subprocess import DEVNULL as devnull
 
+
+def get_ip():
+    '''return IP address from ifconfig.me'''
+    try:
+        res = requests.get("https://ifconfig.me/ip", timeout=1)
+        return res.text
+    except:
+        return " Couldn't resolve IP"
+
+
+def get_geodata(ip):
+    '''return geolocation data for public IP from whatsmyip.org'''
+    url = 'https://www.whatsmyip.org/data/ip-geo-location.php'
+    payload = {'geoipinput': ip}
+    headers = {'Content-Type':'application/x-www-form-urlencoded',
+               'Referer':'https://www.whatsmyip.org/ip-geo-location/?ip=%s' % ip
+    }
+    try:
+        resp = requests.post(url, data=payload, headers=headers, timeout=4)
+    except:
+        return " geoip timeout"
+
+    return resp.text
+
+
 def format_output(res_string):
+    '''extract and format geodata'''
     city         = res_string.split('|')[0]
     state_code   = res_string.split('|')[1]
     country_code = res_string.split('|')[2]
 
     if country_code != '?':
         if city != '?':
-            return " %s, %s" % (city, country_code)
+            return "%s, %s" % (city, country_code)
         else:
-            return " %s" % country_code
+            return "%s" % country_code
     else:
-        return " location unknown"
+        return "location unknown"
+
 
 def main():
+    '''get public ip, and associated geolocation data'''
+    ip = get_ip()
+    geodata = get_geodata(ip)
+    location = format_output(geodata)
+
     # vpn processes to look for
-    #TODO make this an argument
     vpn_processes = "openvpn|openconnect"
 
     # check for running vpn processes running
     if subprocess.run(["pgrep", vpn_processes], stdout=devnull).returncode == 0:
-
-        # get IP address from https://ifconfig.me
-        try:
-            res = requests.get("https://ifconfig.me/ip", timeout=1)
-            ip = res.text
-        except:
-            print(" Couldn't resolve IP")
-            sys.exit(1)
-
-
-        # geolocate public IP
-        url = 'https://www.whatsmyip.org/data/ip-geo-location.php'
-        payload = {'geoipinput': ip}
-        headers = {'Content-Type':'application/x-www-form-urlencoded',
-                   'Referer':'https://www.whatsmyip.org/ip-geo-location/?ip=%s' % ip
-        }
-        try:
-            resp = requests.post(url, data=payload, headers=headers, timeout=4)
-        except:
-            print(" geoip timeout")
-            sys.exit(1)
-
-        print(format_output(resp.text))
-
+        print("",  location)
     else:
-        print("  OFF")
+        print("", location)
 
 if __name__ == "__main__":
     main()
